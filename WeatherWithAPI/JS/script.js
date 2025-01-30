67c5c6b6d1efc56e7cff2c2aad749b2ef265eeb9
Luiz Henrique Cervantes <luizcervantes2002@gmail.com>
2025-01-30 08:57:22 -0300

    all functions working

--- WeatherWithAPI/JS/script.js
+++ WeatherWithAPI/JS/script.js
@@ -1,4 +1,4 @@
-const apiKey = "";
+const apiKey = "4c755ccee32b2cf2692c1ed1dc409363";
 const apiCountryUrl = "https://flagcdn.com/16x12/"
 
 const cityInput = document.querySelector("#city-input")
@@ -16,19 +16,33 @@ const windElement = document.querySelector("#wind span")
 const getWeatherData = async (city) => {
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
-    const res = await fetch(apiWeatherURL);
-    const data = await res.json();
+    try{
+        const res = await fetch(apiWeatherURL);
+        if(!res.ok){
+            throw new Error(`Response status: ${Response.status}`);
+        }
 
-    return data;
+        const data = await res.json();
+        console.log(data);
+        return data;
+    } catch (error){
+        console.error(error.message)
+    }
+    
 };
 
 const showWeatherData = async (city) => {
-    const data = await getWeatherURL(city);
+
+    const data = await getWeatherData(city);
     cityElement.innerText = data.name;
-    tempElement.innerText = parseInt(data.main.temp)
+    tempElement.innerText = parseInt(data.main.temp) + `°C`
     descElement.innerText = data.weather[0].description;
     weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-    countryElement.setAttribute("src", apiCountryUrl + data.sys.country + `.png`);
+
+    const countryFlag = data.sys.country.toLowerCase();
+
+    countryElement.setAttribute("src", `https://flagcdn.com/16x12/${countryFlag}.png`) ;
+
     humidityElement.innerText = `${data.main.humidity}%`;
     windElement.innerText = `${data.wind.speed}km/h`;
 
@@ -38,5 +52,7 @@ const showWeatherData = async (city) => {
 searchBtn.addEventListener("click", (e) => { 
     e.preventDefault();
     const city = cityInput.value;
-    console.log(city);
+    showWeatherData(city);
+
+    
 })
